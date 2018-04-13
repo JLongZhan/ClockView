256e93a0e3f880e5f96d445a47ebda126313d64e
jlow <1138060860@qq.com>
2018-04-13 09:31:55 +0800

    add custom attrs

--- app/src/main/java/com/beacon/clockview/ClockView.java
+++ app/src/main/java/com/beacon/clockview/ClockView.java
@@ -1,16 +1,21 @@
 package com.beacon.clockview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.EmbossMaskFilter;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.RadialGradient;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 import java.util.Calendar;
@@ -25,12 +30,8 @@ import java.util.Locale;
 
 public class ClockView extends View {
 
-    private int mWidth;
-    private final static int DEFAULT_SIZE = 300;
     private static final String TAG = "ClockView";
-    private Paint mClockPaint;
-    private int mClockWidth = 12;
-    private final static int DEFAULT_COLOR = Color.BLACK;
+    private final static int DEFAULT_SIZE = 300;
     /**
      * 每一秒秒针要移动的角度
      */
@@ -53,13 +54,32 @@ public class ClockView extends View {
     private float hour;
     private float minute;
     private float second;
+    private int secondHandColor;
+    private int minuteHandColor;
+    private int hourHandColor;
+    /**
+     * 钟表内的颜色
+     */
+    private int clockColor;
+    /**
+     * 钟表外圈圆的颜色
+     */
+    private int clockCircleColor;
+    /**
+     * 钟表中心点的颜色
+     */
+    private int clockCenterPointColor;
+    private float clockCircleWidth;
     private Paint mSecondPaint;
     private Paint mMinutePaint;
     private Paint mHourPaint;
+    private Paint mClockCirclePaint;
     private Paint mPointPaint;
     private RectF mHourRect;
     private RectF mMinuteRect;
     private RectF mSecondRect;
+    private Paint mClockPaint;
+    private int mClockSize;
 
     public ClockView(Context context) {
         this(context, null);
@@ -71,6 +91,20 @@ public class ClockView extends View {
 
     public ClockView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ClockView, defStyleAttr, 0);
+        secondHandColor = ta.getColor(R.styleable.ClockView_secondHandColor, Color.RED);
+        minuteHandColor = ta.getColor(R.styleable.ClockView_minuteHandColor, Color.BLACK);
+        hourHandColor = ta.getColor(R.styleable.ClockView_hourHandColor, Color.BLACK);
+        clockColor = ta.getColor(R.styleable.ClockView_clockColor, Color.LTGRAY);
+        clockCircleColor = ta.getColor(R.styleable.ClockView_clockCircleColor, Color.BLACK);
+        clockCenterPointColor = ta.getColor(R.styleable.ClockView_clockCenterPointColor, Color.parseColor("#F00000"));
+        clockCircleWidth = ta.getDimension(R.styleable.ClockView_clockCircleWidth, TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 6, context.getResources().getDisplayMetrics()
+        ));
+
+        ta.recycle();
+
         init();
     }
 
@@ -90,38 +124,39 @@ public class ClockView extends View {
     private void init() {
 
         mClockPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mClockPaint.setStyle(Paint.Style.STROKE);
-        mClockPaint.setColor(DEFAULT_COLOR);
-        mClockPaint.setStrokeWidth(mClockWidth);
+        mClockPaint.setStyle(Paint.Style.FILL);
+        mClockPaint.setColor(clockColor);
+
+        mClockCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mClockCirclePaint.setStyle(Paint.Style.STROKE);
+        mClockCirclePaint.setColor(clockCircleColor);
+        mClockCirclePaint.setStrokeWidth(clockCircleWidth);
 
         mHourPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mHourPaint.setColor(Color.BLACK);
+        mHourPaint.setColor(hourHandColor);
         mHourPaint.setStyle(Paint.Style.STROKE);
         mHourPaint.setStrokeWidth(6);
 
         mMinutePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mMinutePaint.setColor(Color.BLACK);
+        mMinutePaint.setColor(minuteHandColor);
         mMinutePaint.setStyle(Paint.Style.STROKE);
         mMinutePaint.setStrokeWidth(4);
 
         mSecondPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mSecondPaint.setColor(Color.RED);
+        mSecondPaint.setColor(secondHandColor);
         mSecondPaint.setStyle(Paint.Style.STROKE);
         mSecondPaint.setStrokeWidth(2);
 
         mPointPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPointPaint.setStrokeCap(Paint.Cap.ROUND);
         mPointPaint.setStrokeWidth(16);
-        mPointPaint.setColor(Color.parseColor("#F00000"));
+        mPointPaint.setColor(clockCenterPointColor);
 
         mHourRect = new RectF();
-
         mMinuteRect = new RectF();
-
         mSecondRect = new RectF();
 
         Calendar calendar = Calendar.getInstance(Locale.CHINA);
-        calendar.setTimeInMillis(System.currentTimeMillis());
         hour = calendar.get(Calendar.HOUR);
         minute = calendar.get(Calendar.MINUTE);
         second = calendar.get(Calendar.SECOND);
@@ -144,31 +179,34 @@ public class ClockView extends View {
         if (heightMode != MeasureSpec.EXACTLY) {
             height = DEFAULT_SIZE;
         }
-        setMeasuredDimension(width, height);
+        mClockSize = Math.min(width, height);
+        setMeasuredDimension(mClockSize, mClockSize);
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
 
-        mSecondRect.left = mSecondRect.top = getMeasuredHeight() / 8;
-        mSecondRect.bottom = mSecondRect.right = getMeasuredWidth() - mSecondRect.left;
+        canvas.drawCircle(mClockSize / 2, mClockSize / 2, mClockSize / 2 - clockCircleWidth, mClockPaint);
+
+        mSecondRect.left = mSecondRect.top = mClockSize / 8;
+        mSecondRect.bottom = mSecondRect.right = mClockSize - mSecondRect.left;
         canvas.drawArc(mSecondRect, second, 0, true, mSecondPaint);
 
-        mMinuteRect.left = mMinuteRect.top = getMeasuredHeight() / 5;
-        mMinuteRect.bottom = mMinuteRect.right = getMeasuredWidth() - mMinuteRect.left;
+        mMinuteRect.left = mMinuteRect.top = mClockSize / 5;
+        mMinuteRect.bottom = mMinuteRect.right = mClockSize - mMinuteRect.left;
         canvas.drawArc(mMinuteRect, minute, 0, true, mMinutePaint);
 
-        mHourRect.left = mHourRect.top = getMeasuredHeight() / 4;
-        mHourRect.bottom = mHourRect.right = getMeasuredWidth() - mHourRect.left;
+        mHourRect.left = mHourRect.top = mClockSize / 4;
+        mHourRect.bottom = mHourRect.right = mClockSize - mHourRect.left;
         canvas.drawArc(mHourRect, hour, 0, true, mHourPaint);
 
-        canvas.drawPoint(getMeasuredHeight() / 2, getMeasuredHeight() / 2, mPointPaint);
+        canvas.drawPoint(mClockSize / 2, mClockSize / 2, mPointPaint);
 
 
-        mClockPaint.setShader(new LinearGradient(getMeasuredHeight() / 2, 0, getMeasuredWidth() / 2,
-                getMeasuredHeight(), Color.parseColor("#777777"), Color.BLACK, Shader.TileMode.CLAMP));
-        canvas.drawCircle(getMeasuredHeight() / 2, getMeasuredHeight() / 2, getMeasuredHeight() / 2 - mClockWidth, mClockPaint);
+        mClockCirclePaint.setShader(new LinearGradient(mClockSize / 2, 0, mClockSize / 2,
+                mClockSize, Color.GRAY, Color.BLACK, Shader.TileMode.CLAMP));
+        canvas.drawCircle(mClockSize / 2, mClockSize / 2, mClockSize / 2 - clockCircleWidth, mClockCirclePaint);
 
         mHandler.sendEmptyMessageDelayed(0, 1000);
     }
